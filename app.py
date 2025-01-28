3d71285b65c19c36e5944052fab2abe60a182799
abhishek-shivale <abhishekshivale45@gmail.com>
2025-01-28 17:47:52 +0530

    added nove_chapter

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # from scrapper.top_novel import *
 from scrapper.novel_info import get_novel_info
 from scrapper.novel_chapters_list import get_novel_chapters_list
+from scrapper.nove_chapter_info import get_novel_chapter_info
 # home()
 
 # get novel_ranking_list url from novel_ranking_list from parametere
@@ -10,4 +11,8 @@ from scrapper.novel_chapters_list import get_novel_chapters_list
 
 # print(get_novel_info("https://ranobes.top/novels/133485-lord-of-the-mysteries-v812312.html"))
 
-print(get_novel_chapters_list('https://ranobes.top/chapters/1206735', 12))
+# print(get_novel_chapters_list('https://ranobes.top/chapters/1206735', 12))
+
+url = "https://ranobes.top/lord-of-the-mysteries-v812312-133485/2067366.html"
+
+print(get_novel_chapter_info(url))
